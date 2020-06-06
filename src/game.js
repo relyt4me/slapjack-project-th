bc45363eb71edb09ef527e9d22ea23ea90512770
Tyler Haglund <relyt4me@gmail.com>
2020-06-06 12:24:22 -0600

    Create shuffle Deck method for Game class with randomCard helper function

--- src/game.js
+++ src/game.js
@@ -4,5 +4,15 @@ class Game {
     this.player1 = new Player(player1Name);
     this.player2 = new Player(player2Name);
   };
+  shuffleDeck() {
+    var shuffledCards = [];
+    for (var i = 0; i < 52; i++) {
+      shuffledCards.push(this.cards.splice(this.randomCard(this.cards), 1)[0])
+    };
+    this.cards = shuffledCards;
+  };
 
+  randomCard(setOfCards) {
+    return Math.floor(Math.random() * setOfCards.length);
+  };
 };
--- src/main.js
+++ src/main.js
@@ -1,2 +1,4 @@
 var game = new Game(cards, 'tyler', 'kaleb');
-console.log(game);
+console.log('Full game: ' + game);
+console.log('player1: ' + game.player1);
+console.log('player2: ' + game.player2);
