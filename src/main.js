8aeeb4bfbd0126b9d45140b9a22cf4882c12decc
Tyler Haglund <relyt4me@gmail.com>
2020-06-07 14:31:26 -0600

    Create eventlistener and handler function for qfjp keys

--- src/main.js
+++ src/main.js
@@ -1,39 +1,57 @@
-var game = new Game(cards, 'tyler', 'kaleb');
+var game = new Game(cards, 'Player One', 'Player 2');
 
-game.redeal(cards);
+window.onload = game.redeal(cards);
 
-function tylerPlay() {
-  game.checkPlay(game.player1, game.player2);
-  displayTopThree();
-};
+document.addEventListener('keypress', controlHandler);
 
-function kalebPlay() {
-  game.checkPlay(game.player2, game.player1);
-  displayTopThree();
-};
-
-function kalebSlapNorm() {
-  game.slapCentralCardsNormal(game.player2, game.player1);
-};
-
-function tylerSlapNorm() {
-  game.slapCentralCardsNormal(game.player1, game.player2);
-};
-
-function kalebSlapSD() {
-  game.slapCentralCardsSuddenDeath(game.player2, game.player1);
-};
-
-function tylerSlapSD() {
-  game.slapCentralCardsSuddenDeath(game.player1, game.player2);
-};
-
-function displayTopThree() {
-  if (game.centralCards.length === 1) {
-    console.log(game.centralCards[0].cardNum);
-  } else if (game.centralCards.length === 2) {
-    console.log(game.centralCards[0].cardNum + ' ' + game.centralCards[1].cardNum);
-  } else {
-    console.log(game.centralCards[0].cardNum + ' ' + game.centralCards[1].cardNum + ' ' + game.centralCards[2].cardNum);
+function controlHandler(event) {
+  if (event.code === 'KeyQ') {
+    console.log('q');
+    player1Play();
+  } else if (event.code === 'KeyP') {
+    console.log('p');
+    player2Play();
+  } else if (event.code === 'KeyF') {
+    console.log('f');
+    player1Slap();
+  } else if (event.code === 'KeyJ') {
+    console.log('j');
+    player2Slap();
   };
-}
+};
+
+// function tylerPlay() {
+//   game.checkPlay(game.player1, game.player2);
+//   displayTopThree();
+// };
+//
+// function kalebPlay() {
+//   game.checkPlay(game.player2, game.player1);
+//   displayTopThree();
+// };
+//
+// function kalebSlapNorm() {
+//   game.slapCentralCardsNormal(game.player2, game.player1);
+// };
+//
+// function tylerSlapNorm() {
+//   game.slapCentralCardsNormal(game.player1, game.player2);
+// };
+//
+// function kalebSlapSD() {
+//   game.slapCentralCardsSuddenDeath(game.player2, game.player1);
+// };
+//
+// function tylerSlapSD() {
+//   game.slapCentralCardsSuddenDeath(game.player1, game.player2);
+// };
+//
+// function displayTopThree() {
+//   if (game.centralCards.length === 1) {
+//     console.log(game.centralCards[0].cardNum);
+//   } else if (game.centralCards.length === 2) {
+//     console.log(game.centralCards[0].cardNum + ' ' + game.centralCards[1].cardNum);
+//   } else {
+//     console.log(game.centralCards[0].cardNum + ' ' + game.centralCards[1].cardNum + ' ' + game.centralCards[2].cardNum);
+//   };
+// }
